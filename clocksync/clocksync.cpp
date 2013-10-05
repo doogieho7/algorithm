1f9a30754b892aac040f3b9ae5491a58b7a7d9fc
Jiho Ma <doogieho7@gmail.com>
2013-10-05 21:46:53 +0900

    clocksync finish

--- clocksync/clocksync.cpp
+++ clocksync/clocksync.cpp
@@ -92,7 +92,106 @@ static void clocksync_count(int switch_click_cur) {
 	}
 }
 
+//책 코드 start
+const int INF = 9999, SWITCHES = 10, CLOCKS = 16;
+int clocks[CLOCKS];
 
+//문자열 사용하면 +1 해줘야함.
+const char linked[SWITCHES][CLOCKS+1] = {
+//0123456789012345
+ "xxx.............",	//0
+ "...x...x.x.x....",	//1
+ "....x.....x...xx",	//2
+ "x...xxxx........",	//3
+ "......xxx.x.x...",	//4
+ "x.x...........xx",	//5
+ "...x..........xx",	//6
+ "....xx.x......xx",	//7
+ ".xxxxx..........",	//8
+ "...xxx...x...x.."		//9
+};
+
+//전역 변수 초기화 
+static void init2() {
+	int i;
+
+	for (int i = 0; i < CLOCKS; ++i)
+		clocks[i] = 0;
+
+}
+
+//모든 시계가 12시를 가르키고 있는지 확인한다.
+bool areAligned(void) {
+	bool aligned = true;
+
+	for (int i = 0; i < CLOCKS; ++i) {
+		if (clocks[i] != 12) {
+			aligned = false;
+			break;
+		}
+	}
+
+	return aligned;
+}
+
+//swtch번 스위치를 누른다.
+void push(int swtch) {
+	for (int clock = 0; clock < CLOCKS; ++clock) {
+		if (linked[swtch][clock] == 'x') {
+			clocks[clock] += 3;
+
+			if (clocks[clock] == 15)
+				clocks[clock] = 3;
+		}
+	}
+}
+
+//swtc: 이번에 누를 스위치의 번호
+int solve(int swtch) {
+	if (swtch == SWITCHES)
+		return areAligned() ? 0 : INF;
+
+	//이 스위치를 0번 누르는 경우부터 세 번 누르는 경우까지를 모두 시도한다.
+	int ret = INF;
+	for (int cnt = 0; cnt < 4; ++cnt) {
+		// 아무거도 안누르는 경우 처리하기 위해서 push() 함수가 뒤에 있음
+		ret = min(ret, cnt + solve(swtch + 1));
+		push(swtch);
+	}
+
+	//push(swtch)가 네번 호출되었으니 clocks는 원래와 같은 상태가 된다.
+	return ret;
+}
+
+int main() {
+	ifstream in(input);
+	if (!in) {
+		cout << "fail to open file" << endl;
+		return -1;
+	}
+
+	int tcase;
+	in >> tcase;
+
+	while (tcase-- > 0) {
+		init2();
+
+		//read times data 
+		for (int i = 0; i < CLOCK_COUNT; ++i)
+			in >> clocks[i];
+
+		int ret = solve(0);
+		if (ret == INF)
+			cout << "-1" << endl;
+		else
+			cout << ret << endl;
+	}
+
+	return 0;
+}
+//책 소스 끝
+
+#if 0
 int main() {
 	ifstream in(input);
 	if (!in) {
@@ -117,8 +216,14 @@ int main() {
 			cout << "-1" << endl;
 		else
 			cout << switch_click_min << endl;
+
+		int ret = solve(0);
+		if (ret == INF)
+			cout << "-1" << endl;
+		else
+			cout << ret << endl;
 	}
 
 	return 0;
 }
-
+#endif
